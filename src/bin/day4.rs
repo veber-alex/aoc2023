ab086e16cc434e1ae03dfc7b7674c4a8244465d0
Alex Veber <alexve@nvidia.com>
2023-12-04 10:37:00 +0200

    Simplify

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -7,68 +7,42 @@ fn main() {
 
 #[derive(Debug)]
 struct Card {
-    winning_numbers: Vec<u32>,
-    numbers: Vec<u32>,
+    score: usize,
     copies: u32,
 }
 
 fn parse(input: &str) -> Vec<Card> {
-    let mut cards = vec![];
-    for line in input.lines() {
-        let (_, data) = line.split_once(':').unwrap();
-        let (win, nums) = data.split_once('|').unwrap();
-        let winning_numbers = win
-            .trim()
-            .split_ascii_whitespace()
-            .map(|n| n.parse().unwrap())
-            .collect();
-        let numbers = nums
-            .trim()
-            .split_ascii_whitespace()
-            .map(|n| n.parse().unwrap())
-            .collect();
-        cards.push(Card {
-            winning_numbers,
-            numbers,
-            copies: 1,
+    let mut winning_numbers = vec![];
+    input
+        .lines()
+        .map(|line| {
+            let (_, data) = line.split_once(':').unwrap();
+            let (win, nums) = data.split_once('|').unwrap();
+            winning_numbers.extend(win.trim().split_ascii_whitespace());
+            let score = nums
+                .trim()
+                .split_ascii_whitespace()
+                .filter(|n| winning_numbers.contains(n))
+                .count();
+            winning_numbers.clear();
+            Card { score, copies: 1 }
         })
-    }
-
-    cards
+        .collect()
 }
 
 fn part1(input: &str) -> u32 {
-    let cards = parse(input);
-    let mut sum = 0;
-    let mut score = 0;
-    for card in cards {
-        for num in card.numbers {
-            if card.winning_numbers.contains(&num) {
-                if score == 0 {
-                    score = 1
-                } else {
-                    score *= 2;
-                }
-            }
-        }
-        sum += score;
-        score = 0
-    }
-
-    sum
+    parse(input)
+        .iter()
+        .map(|card| card.score.checked_sub(1).map(|n| 1 << n).unwrap_or(0))
+        .sum()
 }
 
 fn part2(input: &str) -> u32 {
     let mut cards = parse(input);
     for idx in 0..cards.len() {
-        let card = &mut cards[idx];
-        let score = card
-            .winning_numbers
-            .iter()
-            .filter(|win| card.numbers.contains(win))
-            .count();
+        let card = &cards[idx];
         let card_copies = card.copies;
-        for next in 1..=score {
+        for next in 1..=card.score {
             cards[idx + next].copies += card_copies
         }
     }
