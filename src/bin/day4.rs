2a8ecaccf318676e199ebadcaefb8f1d6b022c13
Alex Veber <alexve@nvidia.com>
2023-12-04 21:07:30 +0200

    Cleanup

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -18,9 +18,8 @@ fn parse(input: &str) -> Vec<Card> {
         .map(|line| {
             let (_, data) = line.split_once(':').unwrap();
             let (win, nums) = data.split_once('|').unwrap();
-            winning_numbers.extend(win.trim().split_ascii_whitespace());
+            winning_numbers.extend(win.split_ascii_whitespace());
             let score = nums
-                .trim()
                 .split_ascii_whitespace()
                 .filter(|n| winning_numbers.contains(n))
                 .count();
@@ -33,19 +32,20 @@ fn parse(input: &str) -> Vec<Card> {
 fn part1(input: &str) -> u32 {
     parse(input)
         .iter()
-        .map(|card| card.score.checked_sub(1).map(|n| 1 << n).unwrap_or(0))
+        .filter_map(|card| card.score.checked_sub(1).map(|n| 1 << n))
         .sum()
 }
 
 fn part2(input: &str) -> u32 {
     let mut cards = parse(input);
-    for idx in 0..cards.len() {
-        let card = &cards[idx];
-        let card_copies = card.copies;
-        for next in 1..=card.score {
-            cards[idx + next].copies += card_copies
-        }
-    }
-
-    cards.iter().map(|card| card.copies).sum()
+    (0..cards.len())
+        .map(|idx| {
+            let card = &cards[idx];
+            let card_copies = card.copies;
+            for next in 1..=card.score {
+                cards[idx + next].copies += card_copies
+            }
+            card_copies
+        })
+        .sum()
 }
