145bd8acf98b1d8f1ac499415a68756113332a53
Alex Veber <alexve@nvidia.com>
2023-12-08 10:03:03 +0200

    Simplify

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,4 +1,3 @@
-use std::cmp::Ordering;
 use std::iter::zip;
 
 fn main() {
@@ -8,7 +7,7 @@ fn main() {
     assert_eq!(part2(input), 254412181);
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 enum HandKind {
     HighCard,
     OnePair,
@@ -21,18 +20,15 @@ enum HandKind {
 
 use HandKind::*;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Hand {
-    cards: [u8; 5],
     kind: HandKind,
+    cards: [u8; 5],
     bid: u32,
 }
 
 fn solve(mut hands: Vec<Hand>) -> u32 {
-    hands.sort_by(|a, b| match a.kind.cmp(&b.kind) {
-        Ordering::Equal => a.cards.cmp(&b.cards),
-        ord => ord,
-    });
+    hands.sort_unstable();
 
     hands
         .iter()
@@ -63,26 +59,16 @@ fn part1(input: &str) -> u32 {
             }
 
             let mut kind = HighCard;
-            let mut three = false;
-            let mut two = 0;
             for count in counter {
-                match count {
-                    5 => kind = FiveOfAKind,
-                    4 => kind = FourOfAKind,
-                    3 => three = true,
-                    2 => two += 1,
-                    0 | 1 => {}
-                    _ => unreachable!(),
-                }
-            }
-
-            if kind == HighCard {
-                kind = match (three, two) {
-                    (false, 0) => HighCard,
-                    (false, 1) => OnePair,
-                    (false, 2) => TwoPair,
-                    (true, 0) => ThreeOfAKind,
-                    (true, 1) => FullHouse,
+                kind = match (count, kind) {
+                    (5, _) => FiveOfAKind,
+                    (4, _) => FourOfAKind,
+                    (3, HighCard) => ThreeOfAKind,
+                    (3, OnePair) => FullHouse,
+                    (2, HighCard) => OnePair,
+                    (2, OnePair) => TwoPair,
+                    (2, ThreeOfAKind) => FullHouse,
+                    (0 | 1, _) => continue,
                     _ => unreachable!(),
                 };
             }
@@ -115,50 +101,36 @@ fn part2(input: &str) -> u32 {
                 counter[*card as usize] += 1;
             }
 
-            let jokers = counter[0];
-            let mut five = false;
-            let mut four = false;
-            let mut three = false;
-            let mut two = 0;
+            let mut kind = HighCard;
             for count in &counter[1..] {
-                match count {
-                    5 => five = true,
-                    4 => four = true,
-                    3 => three = true,
-                    2 => two += 1,
-                    0 | 1 => {}
+                kind = match (count, kind) {
+                    (5, _) => FiveOfAKind,
+                    (4, _) => FourOfAKind,
+                    (3, HighCard) => ThreeOfAKind,
+                    (3, OnePair) => FullHouse,
+                    (2, HighCard) => OnePair,
+                    (2, OnePair) => TwoPair,
+                    (2, ThreeOfAKind) => FullHouse,
+                    (0 | 1, _) => continue,
                     _ => unreachable!(),
-                }
+                };
             }
 
-            let kind = if five || four && jokers == 1 {
-                FiveOfAKind
-            } else if four {
-                FourOfAKind
-            } else if three {
-                match (two, jokers) {
-                    (0, 2) => FiveOfAKind,
-                    (0, 1) => FourOfAKind,
-                    (1, 0) => FullHouse,
-                    (0, 0) => ThreeOfAKind,
-                    _ => unreachable!(),
-                }
-            } else {
-                match (two, jokers) {
-                    (0, 0) => HighCard,
-                    (0, 1) => OnePair,
-                    (0, 2) => ThreeOfAKind,
-                    (0, 3) => FourOfAKind,
-                    (0, 4) => FiveOfAKind,
-                    (0, 5) => FiveOfAKind,
-                    (1, 0) => OnePair,
-                    (1, 1) => ThreeOfAKind,
-                    (1, 2) => FourOfAKind,
-                    (1, 3) => FiveOfAKind,
-                    (2, 0) => TwoPair,
-                    (2, 1) => FullHouse,
-                    _ => unreachable!(),
-                }
+            let jokers = counter[0];
+            kind = match (jokers, kind) {
+                (0, _) => kind,
+                (1, FourOfAKind) => FiveOfAKind,
+                (1, ThreeOfAKind) => FourOfAKind,
+                (1, TwoPair) => FullHouse,
+                (1, OnePair) => ThreeOfAKind,
+                (1, HighCard) => OnePair,
+                (2, ThreeOfAKind) => FiveOfAKind,
+                (2, OnePair) => FourOfAKind,
+                (2, HighCard) => ThreeOfAKind,
+                (3, OnePair) => FiveOfAKind,
+                (3, HighCard) => FourOfAKind,
+                (4 | 5, _) => FiveOfAKind,
+                _ => unreachable!(),
             };
 
             Hand { cards, kind, bid }
